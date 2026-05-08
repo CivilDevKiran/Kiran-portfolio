b0c05c309d014df9bb6ea5d9fce7219ee4dc99c3
Mohammad Rayyan Adhoni <mohammad.adhoni.2023@uni.strath.ac.uk>
2026-05-08 16:59:19 +0100

    Fixed layout and Form submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('contactForm').addEventListener('submit', async function
     const form = event.target;
     const formData = new FormData(form);
 
-    if (formData.get('access_key') === 'YOUR_WEB3FORMS_KEY') {
+    if (formData.get('access_key') === 'e6d360cf-bccd-4730-abc1-77bc7b5fe777') {
         alert('Add your Web3Forms access key to enable submissions.');
         return;
     }
